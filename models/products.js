ef6d075fdd63f0b4e609d3255dff6b40c1071a78
merlinoa <merlinoa88@gmail.com>
2016-07-05 21:44:47 -0400

    initial commit

---
+++ models/products.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// blue print for entries in the database
+var schema = new Schema({
+    imagePath: {type: String, required: true},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Product', schema);
